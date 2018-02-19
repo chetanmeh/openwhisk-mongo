bcadee719ea86e2239c46ff165c5a9bf8e93a748
Chetan Mehrotra <chetanm@adobe.com>
2018-02-19 21:23:33 +0530

    Simplify the build command
    Remove calls for disabling test and createKeyStore as apache/incubator-openwhisk#3282 is now merged

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -10,8 +10,7 @@ WHISKDIR="$ROOTDIR/../openwhisk"
 cd $WHISKDIR
 GRADLE_PROJS_SKIP="-x :actionRuntimes:pythonAction:distDocker  -x :actionRuntimes:python2Action:distDocker -x actionRuntimes:swift3.1.1Action:distDocker -x :actionRuntimes:javaAction:distDocker"
 
-# TODO Remove createKeystore
-TERM=dumb ./gradlew install distDocker -PdockerImagePrefix=testing -x test -x createKeystore $GRADLE_PROJS_SKIP
+TERM=dumb ./gradlew install distDocker -PdockerImagePrefix=testing $GRADLE_PROJS_SKIP
 
 cd $ROOTDIR
 TERM=dumb ./gradlew :tests:reportScoverage
